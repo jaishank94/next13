d535f9416019a3c943ec072d93a475cf3ce7f774
Jai Shankar Prasad <jaishankarprasad@Aasthas-MacBook-Pro.local>
2023-09-28 22:23:06 +0530

    Updated the date211

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -10,9 +10,9 @@ const HeroSection = () => {
     <section className="bg-black px-8 md:px-24">
       <div className="grid grid-cols-1 sm:grid-cols-12 py-12">
         <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
+          // initial={{ opacity: 0, scale: 0.5 }}
+          // animate={{ opacity: 1, scale: 1 }}
+          // transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
           <h1 className="text-white mb-4 mt-12 text-4xl sm:text-4xl lg:text-7xl lg:leading-normal font-medium">
